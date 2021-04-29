ddd592c6fee31b6fd5f1573485f3cbfaccd4a304
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2021-04-28 21:52:32 -0500

    almost done

--- main.js
+++ main.js
@@ -1,65 +1,95 @@
 // **THIS IS INCREDIBLY IMPORTANT THAT YOU DO BOTH SECTIONS!!! You will be doing only front-end work in 421 and you need to brush up on your HTML elements**
 
-
 // ***************************
 //          PART ONE
 // ***************************
 // Write a JavaScript program to display the current day and time, start with:
-console.log(new Date)
+console.log(new Date());
 
 const displayDate = () => {
-  const currentDate = new Date()
+  const currentDate = new Date();
 
   document.getElementById("display-element").innerHTML = currentDate;
-}
- 
+};
 
 // Write a JavaScript program to convert a number to a string.
 
+const numToString = (num) => {
+  return String(num);
+};
 
+console.log(numToString(3));
 
 // Write a JavaScript program to convert a string to the number.
 
+const stringToNum = (str) => {
+  return Number(str);
+};
 
+console.log(stringToNum("4"));
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-  // * Boolean
-  // * Null
-  // * Undefined
-  // * Number
-  // * NaN
-  // * String
-  
+// * Boolean
+// * Null
+// * Undefined
+// * Number
+// * NaN
+// * String
 
-  
+const checkType = (type) => {
+  return typeof type;
+};
+
+console.log(checkType(4));
+
 // Write a JavaScript program that adds 2 numbers together.
 
+const addTwo = (num1, num2) => {
+  return num1 + num2;
+};
 
+console.log(addTwo(3, 6));
 
 // Write a JavaScript program that runs only when 2 things are true.
 
+const bothTrue = (check1, check2) => {
+  if (check1 == 5 && check2 == 6) {
+    return console.log("Both true");
+  } else {
+    return console.log("Not both true");
+  }
+};
 
+console.log(bothTrue(4, 6));
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
+const oneTrue = (arg1, arg2) => {
+  if (arg1 == 3 || arg2 == 4) {
+    return console.log("one is true");
+  } else {
+    return console.log("neither is true");
+  }
+};
 
+console.log(oneTrue(3, 4));
+// Write a JavaScript program that runs when both things are not true.
 
-// Write a JavaScript program that runs when both things are not true.  
+const bothNotTrue = (argu1, argu2) => {
+  if ()
+}
 
 // ***************************
 //         PART TWO
 // ***************************
 
-// 1. download Live-Server by Ritwick Dey, 
-// 2. reload VS Code, 
+// 1. download Live-Server by Ritwick Dey,
+// 2. reload VS Code,
 // 3. click the "Go Live" button
 // 4. Go local host 5500 or http://127.0.0.1:5500/index.html to see your web page
 // 5. Or go use the `npm start` command and navigate to localhost:8080 (ctrl + C to close)
-// 6. go to `index.html` 
+// 6. go to `index.html`
 // 7. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
-
-
-
 
 // Additional Resources
 // Video1: https://player.vimeo.com/video/377147232
